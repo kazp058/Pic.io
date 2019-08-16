b448fe8d7a4a296f2f0c8a745122a5bbb7580e79
Franklin <frajordo@espol.edu.ec>
2019-08-16 18:21:39 -0500

    Completado login scene

--- src/Main/MainClass.java
+++ src/Main/MainClass.java
@@ -8,22 +8,44 @@ package Main;
 import Scenes.*;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.*;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Random;
 
 /**
  *
  * @author Franklin
  */
 public class MainClass extends Application {
-
+    Font bits;
+    Background background;
     public static String loginName = "login";
+
+    public MainClass() throws FileNotFoundException {
+        this.bits = Font.loadFont(new FileInputStream("src/Data/fonts/upheavtt.ttf"),20);
+        File dir= new File("src/Data/gifs");
+        File[] files=dir.listFiles();
+        Random rand=new Random();
+        File location=files[rand.nextInt(files.length)];
+        BackgroundImage bgi;
+        Image image=new Image(new FileInputStream(location));
+        bgi = new BackgroundImage(image,
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+                new BackgroundSize(1.0, 1.0, true, true, false, false));
+        this.background=new Background(bgi);
+    }
     
     public void start(Stage primaryStage){
         
         ScreensController myController = new ScreensController();
 
-        ControllableScene loginScene = new LoginRegisterScene();
+        ControllableScene loginScene = new LoginScene(bits,background);
         
         myController.setSize(new Integer[]{1280,720});
         
